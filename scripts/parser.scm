cf005a23919089e308e21572fd71dc2cc4cfde82
Tom Balzer <niebieskitrociny@gmail.com>
2017-11-20 02:50:17 -0600

    Updated parser to start iterating through response.

--- scripts/parser.scm
+++ scripts/parser.scm
@@ -1,63 +1,51 @@
 #!/run/current-system/profile/bin/guile
 !#
 
+(use-modules (sxml simple))
+(use-modules (ice-9 pretty-print))
+
 (define process-response
   (lambda (response)
 	"doc for process-response"
-	(write "processing response")
-	(newline)))
+	(display "processing response")
+	(newline)
+	(let ((root (car response)))
+	 (display
+	  (string-append
+	   "root element:'" (symbol->string root) "'\n"))
+	 (if (eq? root 'response)
+		 ((lambda ()
+			(display "found response - moving to result")
+			(newline)
+			(process-result response)))
+		 (display "not response.\n"))
+	 )
+	(newline)
+	))
 
 (define process-result
   (lambda (result)
 	"doc for process-result"
-	(write "processing result")
+	(display "processing result")
 	(newline)))
 
 (define iterate-data
   (lambda (data)
 	"doc for iterate-data"
-	(if (and (list? data) (< 0 (length data)))
-		((lambda ()
-		  (let ((this (car data)))
-			(define qwer '())
-			(if (list? this)
-				(set! qwer (list-tail (car data) 1))
-				(set! qwer (list-tail data 1))
-				)
-		  
-			(if (eq? this 'project)
-				((lambda ()
-				   (write data)
-				   (newline)))
-				((lambda ()
-				   ;; (write this)
-				   ;; (newline)
-				   (iterate-data qwer)))
-				))
-		  		  
-		  (do ((i 1 (1+ i)))
-		  	  ((and (list? data) (> i (- (length data) 1))))
-		  	(iterate-data (list-ref data i))
-		  	) ; do
-		  )
-		 )
-		;; ((lambda ()
-		;;    ;; (write '--------)
-		;;    ;; (newline)
-		;;    ))
-		
-		) ; (if (< 0 (length data))
-	)
+	(do ((i 1 (1+ i)))
+		((and (list? data) (> i (- (length data) 1))))
+	  (let ((tmp (list-ref data i)))
+		(process-response tmp)
+		)
+	  ) ; do
+	)	
   )
-
-(use-modules (sxml simple))
-(use-modules (ice-9 pretty-print))
 
 (define data-port (open-file "/home/niebie/sc/560-project/scripts/sample-data.xml" "r"))
 (define sxml-data (xml->sxml data-port))
 
 (newline)
-(write 'output:)
+(display "output:")
 (newline)
 (iterate-data sxml-data)
 (newline)
