0179c3fb407cf3e52ee913f08064e0b7e56233af
Brandon Jansen <jansennerd10@ksu.edu>
2017-12-09 15:24:20 -0600

    fillTable example code

--- public/index.php
+++ public/index.php
@@ -9,15 +9,6 @@ echo "Hello " . htmlspecialchars($_SESSION['userdata']['nickname']);
 ?>
 <a href="/logout.php">Log out</a>
 
-<table id="test-table">
-<thead>
-	<tr><th data-sourcecol='col1'>Column 1</th><th data-sourcecol='col2'>Column 2</th><th data-sourcecol='cb'>Callback</th></tr>
-</thead>
-<tbody>
-	<!-- Will be filled by BNTLib. -->
-</tbody>
-</table>
-
 <?php
 require($_SERVER['DOCUMENT_ROOT'] . '/footer.php');
 ?>
