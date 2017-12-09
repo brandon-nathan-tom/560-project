e1a2c7168d49af0d02963497ba93595ef18d8526
Brandon Jansen <jansennerd10@ksu.edu>
2017-12-09 15:20:59 -0600

    AJAX header

---
+++ public/ajax_header.php
@@ -0,0 +1,14 @@
+<?php
+
+session_start();
+
+require($_SERVER['DOCUMENT_ROOT'] . '/../php/database.php');
+
+if(!isset($login_required)) $login_required = true;
+
+if($login_required && !require($_SERVER['DOCUMENT_ROOT'] . '/../php/login.php'))
+{
+	return;
+}
+
+?>
