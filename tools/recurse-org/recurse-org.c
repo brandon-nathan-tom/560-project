fae764f0d52ba48af56c53a035468857541f582b
Tom Balzer <niebieskitrociny@gmail.com>
2017-12-12 22:34:52 -0600

    Updated data collection.

--- tools/recurse-org/recurse-org.c
+++ tools/recurse-org/recurse-org.c
@@ -1,3 +1,5 @@
+#include "basic_auth.h"
+
 #include <jansson.h>
 #include <string.h>
 #include <stdlib.h>
@@ -101,8 +103,8 @@ static char *request(const char *url)
     headers = curl_slist_append(headers, "User-Agent: niebieskitrociny");
 	
     curl_easy_setopt(curl, CURLOPT_HTTPAUTH, (long)CURLAUTH_BASIC);
-    curl_easy_setopt(curl, CURLOPT_USERNAME, "niebieskitrociny");
-	curl_easy_setopt(curl, CURLOPT_PASSWORD, "eat40cookies");
+    curl_easy_setopt(curl, CURLOPT_USERNAME, BASIC_AUTH_USER);
+	curl_easy_setopt(curl, CURLOPT_PASSWORD, BASIC_AUTH_PASS);
 	
     curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
 
@@ -226,7 +228,7 @@ handle_commits(int argc, char **argv){
   json_t *var;
 
 int
-handle_user(const char *user_url, const char *org_name){
+handle_user_org(const char *user_url, const char *org_name){
   MACRO_json_string(email);
   MACRO_json_string(name);
   /* MACRO_json_string(nickname); */
@@ -251,6 +253,7 @@ handle_user(const char *user_url, const char *org_name){
 	SQL_TEXT("select watch_contrib.id, '%.40s', '%.40s'")
 	SQL_TEXT("from watch_contrib")
 	SQL_TEXT("returning id;")
+	/* associate with org */
 	SQL_TEXT("\nwith org_select as (")
 	SQL_TEXT("select * from organizations")
 	SQL_TEXT("where name = '%s'")
@@ -262,6 +265,11 @@ handle_user(const char *user_url, const char *org_name){
 	SQL_TEXT("select org_select.id, contrib_select.id, 'User'")
 	SQL_TEXT("from org_select, contrib_select;");
 
+  if(string_name == NULL){
+	// no point
+	return 1;
+  }
+  
   if(string_email == NULL){
 	string_email = malloc(41 * sizeof(char));
 	snprintf(string_email, 41, "<private>");
@@ -285,7 +293,72 @@ handle_user(const char *user_url, const char *org_name){
 }
 
 int
-handle_members(const char *members_url, const char *org_name){
+handle_user_proj(const char *user_url, const char *proj_name){
+  MACRO_json_string(email);
+  MACRO_json_string(name);
+  /* MACRO_json_string(nickname); */
+  
+  /* printf("url:'%s'\n", user_url); */
+  MACRO_silent_request(user_url);
+  check_or_complain(!json_is_object(request_root),
+					"root is not an object\n",
+					NULL, NULL);
+  
+  MACRO_json_get_string(request_root, email, "email");
+  MACRO_json_get_string(request_root, name, "name");
+  /* MACRO_json_get_string(request_root, nickname, "login"); */
+
+  char statement[] =
+	SQL_TEXT("with watch_contrib as (")
+	SQL_TEXT("    insert into watchables")
+	SQL_TEXT("    default values")
+	SQL_TEXT("    returning id")
+	SQL_TEXT(")")
+	SQL_TEXT("insert into contributors (id, name, email)")
+	SQL_TEXT("select watch_contrib.id, '%.40s', '%.40s'")
+	SQL_TEXT("from watch_contrib")
+	SQL_TEXT("returning id;")
+	/* associate with proj */
+	SQL_TEXT("\nwith proj_select as (")
+	SQL_TEXT("select * from projects")
+	SQL_TEXT("where name = '%s'")
+	SQL_TEXT("), contrib_select as (")
+	SQL_TEXT("select * from contributors")
+	SQL_TEXT("where name = '%s'")
+	SQL_TEXT(")")
+	SQL_TEXT("insert into project_contributors (project_id, contributor_id, role)")
+	SQL_TEXT("select proj_select.id, contrib_select.id, 'User'")
+	SQL_TEXT("from proj_select, contrib_select;");
+
+  if(string_email == NULL){
+	string_email = malloc(41 * sizeof(char));
+	snprintf(string_email, 41, "<private>");
+  }
+  
+  if(string_name == NULL){
+	// no point
+	return 1;
+  }
+  
+  char *buffer = malloc(10000 * sizeof(char));
+  snprintf(buffer,
+		   10000,
+		   statement,
+		   string_name,
+		   string_email,
+		   proj_name,
+		   string_name);
+  printf("%s\n", buffer);
+  free(buffer);
+  /* printf("User data collected:\n"); */
+  /* MACRO_pretty_print_string(name); */
+  /* MACRO_pretty_print_string(email); */
+  /* MACRO_pretty_print_string(nickname); */
+  return 0;
+}
+
+int
+handle_members(const char *members_url, const char *assoc_name, int (*fun_ptr)(const char *, const char *)){
   MACRO_silent_request(members_url);
   /* MACRO_print_request(); */
   check_or_complain(!json_is_array(request_root),
@@ -305,16 +378,196 @@ handle_members(const char *members_url, const char *org_name){
 					  i, NULL);
 	MACRO_json_get_string(data, login, "login");
 	MACRO_json_get_string(data, user_url, "url");
-	handle_user(string_user_url, org_name);
-	return 0;
+	fun_ptr(string_user_url, assoc_name);
   }
   return 0;
 }
 
 int
-handle_repos(const char *repos_url){
-  MACRO_verbose_request(repos_url);
-  MACRO_print_request();
+handle_license(const char *lic_uri, char **lic_name, char **lic_html_url, char **lic_descr){
+  MACRO_silent_request(lic_uri);
+  check_or_complain(!json_is_object(request_root),
+					"lic obj not an obj\n",
+					NULL, NULL);
+
+  MACRO_json_string(name);
+  MACRO_json_string(html_uri);
+  MACRO_json_string(description);
+
+  MACRO_json_get_string(request_root, name, "name");
+  MACRO_json_get_string(request_root, html_uri, "html_url");
+  MACRO_json_get_string(request_root, description, "description");
+
+  *lic_name = string_name;
+  *lic_html_url = string_html_uri;
+  *lic_descr = string_description;
+  return 0;
+}
+
+char *
+clean_value(char *input, int max) {
+  char *temp = malloc(2 * max * sizeof(char));
+  memset(temp, 0, 2 * max * sizeof(char));
+  int temp_i = 0;
+  for(int i=0;
+	  i<max;
+	  i++){
+	if(input[i] == '\''){
+	  temp[temp_i] = '\'';
+	  temp_i++;
+	}
+	
+	temp[temp_i]=input[i];
+	temp_i++;
+  }
+  return temp;
+}
+
+int
+handle_repos(const char *repos_url, const char *org_name){
+  MACRO_silent_request(repos_url);
+
+  check_or_complain(!json_is_array(request_root),
+					"root is not an array\n",
+					NULL, NULL);
+
+  for(int i=0;
+	  i<json_array_size(request_root);
+	  i++){
+	json_t *data;
+	data = json_array_get(request_root, i);
+	check_or_complain(!json_is_object(data),
+					  "repo data %i not an object\n",
+					  i, NULL);
+		
+	MACRO_json_string(name);
+	MACRO_json_string(repo);
+	MACRO_json_string(short_description);
+	MACRO_json_string(description);
+	MACRO_json_string(contributors);
+	MACRO_json_string(homepage);
+  
+	MACRO_json_get_string(data, name, "name");
+	MACRO_json_get_string(data, repo, "git_url");
+	MACRO_json_get_string(data, description, "description");
+	MACRO_json_get_string(data, contributors, "contributors_url");
+	MACRO_json_get_string(data, homepage, "homepage");
+	if(json_is_null(homepage)){
+	  MACRO_json_get_string(data, homepage, "html_url");
+	}
+
+	json_t *license = json_object_get(data, "license");
+	int has_license;
+	char *lic_name, *lic_html_url, *lic_descr;
+	MACRO_json_string(lic_uri);
+	if(!json_is_null(license)){
+	  has_license = 1;
+	  check_or_complain(!json_is_object(license),
+						"license is not an object\n",
+						NULL, NULL);
+
+	  MACRO_json_get_string(license, lic_uri, "url");
+	  if(json_is_null(lic_uri)){
+		has_license = 0;
+	  }else {
+		handle_license(string_lic_uri,
+					   &lic_name,
+					   &lic_html_url,
+					   &lic_descr);
+	  }
+	}
+
+	char statement[] =
+	  SQL_TEXT("with watch_proj as (")
+	  SQL_TEXT("    insert into watchables")
+	  SQL_TEXT("    default values")
+	  SQL_TEXT("    returning id")
+	  SQL_TEXT("), org_select as (")
+	  SQL_TEXT("    select *")
+	  SQL_TEXT("    from organizations")
+	  SQL_TEXT("    where name = '%s'")
+	  SQL_TEXT(")")
+	  SQL_TEXT("insert into projects (id, name, repo, owner_id, short_description , description)")
+	  SQL_TEXT("select watch_proj.id, '%.40s', '%.2000s', org_select.id, '%.40s', '%.2000s'")
+	  SQL_TEXT("from watch_proj, org_select;")
+	  /* add homepage */
+	  SQL_TEXT("with homepage_ins as (")
+	  SQL_TEXT("    insert into websites (name, uri, descr)")
+	  SQL_TEXT("    values ('Homepage', '%.250s', '%.400s')")
+	  SQL_TEXT("    returning id")
+	  SQL_TEXT("), watch_sel as (")
+	  SQL_TEXT("    select * from projects")
+	  SQL_TEXT("    where name = '%.40s'")
+	  SQL_TEXT(")")
+	  SQL_TEXT("insert into watchables_sites (watchable_id, site_id)")
+	  SQL_TEXT("select watch_sel.id, homepage_ins.id")
+	  SQL_TEXT("from watch_sel, homepage_ins;")
+	  ;
+
+	char lic_statement[] =
+	  /* license */
+	  SQL_TEXT("with website_insert as (")
+	  SQL_TEXT("    insert into websites (name, uri, descr)")
+	  SQL_TEXT("    values ('%.40s', '%.250s', '%.400s')")
+	  SQL_TEXT("    returning id")
+	  SQL_TEXT(")")
+	  SQL_TEXT("insert into licenses (name, text_link)")
+	  SQL_TEXT("select '%.40s', website_insert.id")
+	  SQL_TEXT("from website_insert;")
+	  /* link to license */
+	  SQL_TEXT("with proj_sel as (")
+	  SQL_TEXT("    select * from projects")
+	  SQL_TEXT("    where name = '%.40s'")
+	  SQL_TEXT("), lic_sel as (")
+	  SQL_TEXT("    select * from licenses")
+	  SQL_TEXT("    where name = '%.40s'")
+	  SQL_TEXT(")")
+	  SQL_TEXT("insert into project_licenses (project_id, license_id)")
+	  SQL_TEXT("select proj_sel.id, lic_sel.id")
+	  SQL_TEXT("from proj_sel, lic_sel;");
+
+	char *buffer = malloc(10000 * sizeof(char));
+
+	char homepage_str[400];
+	snprintf(homepage_str,
+			 400,
+			 "Homepage for project %s",
+			 string_name);
+	char *cleaned_desc = clean_value(string_description,
+									 strlen(string_description));
+	snprintf(buffer,
+			 10000,
+			 statement,
+			 org_name,
+			 string_name,
+			 string_repo,
+			 cleaned_desc,
+			 cleaned_desc,
+			 string_homepage,
+			 homepage_str,
+			 string_name);
+	printf("%s", buffer);
+	free(cleaned_desc);
+
+	if(has_license){
+	  snprintf(buffer,
+			   10000,
+			   lic_statement,
+			   lic_name,
+			   lic_html_url,
+			   lic_descr,
+			   lic_name,
+			   string_name,
+			   lic_name);
+	  printf("%s", buffer);
+	}
+			 
+	free(buffer);
+
+	handle_members(string_contributors,
+				   string_name,
+				   handle_user_proj);
+  }
   return 0;
 }
 
@@ -356,7 +609,10 @@ handle_org(int argc, char **argv){
 	
 	MACRO_json_get_string(request_root, name, "name");
 	MACRO_json_get_string(request_root, repos_url, "repos_url");
-	MACRO_json_get_string(request_root, homepage, "html_url");
+	MACRO_json_get_string(request_root, homepage, "blog");
+	if(json_is_null(homepage)){
+	  MACRO_json_get_string(request_root, homepage, "html_url");
+	}
 	// this has {/member} in it - useless.
 	//MACRO_json_get_string(request_root, users_url, "public_members_url");
 	MACRO_json_get_string(request_root, description, "description");
@@ -403,8 +659,8 @@ handle_org(int argc, char **argv){
 	/* MACRO_pretty_print_string(members_url); */
 	/* MACRO_pretty_print_string(repos_url) */;
 
-	/* handle_members(string_members_url, string_name); */
-	handle_repos(string_repos_url);
+	handle_members(string_members_url, string_name, handle_user_org);
+	handle_repos(string_repos_url, string_name);
 	
 	return 0;
   } else {
@@ -425,6 +681,6 @@ main(int argc, char **argv) {
   /* 	  i++){ */
   /* 	printf("arg[%d]:%s\n", i, argv[i]); */
   /* } */
-
+  
   return handle_org(argc, argv);
 }
